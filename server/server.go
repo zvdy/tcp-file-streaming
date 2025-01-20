092a7d24835af051c81d40c7ccaa75e3bc08cc15
zvdy <cristian.030902@gmail.com>
2025-01-20 11:50:38 +0100

    add: features and clean code

--- server/server.go
+++ server/server.go
@@ -2,42 +2,92 @@ package server
 
 import (
 	"bytes"
+	"crypto/rand"
 	"encoding/binary"
 	"fmt"
 	"io"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 type FileServer struct {
+	Port string
 }
 
 func (fs *FileServer) Start() {
-	ln, err := net.Listen("tcp", ":8080")
+	ln, err := net.Listen("tcp", fs.Port)
+	fmt.Println("Server started on", fs.Port)
 	if err != nil {
 		log.Fatal(err)
 	}
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			log.Fatal(err)
+	defer ln.Close()
+
+	// Handle graceful shutdown
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		for {
+			conn, err := ln.Accept()
+			if err != nil {
+				log.Println("Error accepting connection:", err)
+				continue
+			}
+			go fs.handleConnection(conn)
 		}
-		go fs.handleConnection(conn)
-	}
+	}()
+
+	<-stop
+	log.Println("Shutting down server...")
 }
 
 func (fs *FileServer) handleConnection(conn net.Conn) {
+	defer conn.Close()
 	buf := new(bytes.Buffer)
 	for {
 		var size int64
-		binary.Read(conn, binary.LittleEndian, &size)
+		err := binary.Read(conn, binary.LittleEndian, &size)
+		if err != nil {
+			if err == io.EOF {
+				log.Println("Connection closed by client")
+				return
+			}
+			log.Println("Error reading size:", err)
+			return
+		}
 		n, err := io.CopyN(buf, conn, size)
 		if err != nil {
-			log.Println(err)
+			if err == io.EOF {
+				log.Println("Connection closed by client")
+				return
+			}
+			log.Println("Error reading data:", err)
 			return
 		}
 
-		fmt.Println(buf.Bytes())
-		fmt.Printf("Received %d bytes\n", n)
+		// Generate a random unique ID string
+		randomID := make([]byte, 16)
+		_, err = rand.Read(randomID)
+		if err != nil {
+			log.Println("Error generating random ID:", err)
+			return
+		}
+		fileName := fmt.Sprintf("%d_%x", size, randomID)
+
+		// Log the received data
+		fmt.Printf("Received %d bytes and saved to memory buffer with ID %s\n", n, fileName)
+
+		// Notify the client that the server has finished processing
+		_, err = conn.Write([]byte("done"))
+		if err != nil {
+			log.Println("Error notifying client:", err)
+			return
+		}
+
+		// Close the connection
+		return
 	}
 }
