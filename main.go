c66d3b5bb4582c420e9e60ccda122d53fe5d67c4
zvdy <cristian.030902@gmail.com>
2025-01-21 19:23:54 +0100

    feat: implement http service call

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/zvdy/tcp-file-streaming/server"
 )
 
 func main() {
-	server := &server.FileServer{Port: ":8080"}
-	server.Start()
+	tcpPort := os.Getenv("FILE_SERVER_PORT")
+	if tcpPort == "" {
+		log.Fatal("FILE_SERVER_PORT environment variable is not set")
+	}
+
+	httpPort := os.Getenv("HTTP_PORT")
+	if httpPort == "" {
+		log.Fatal("HTTP_PORT environment variable is not set")
+	}
+
+	fileServer := &server.FileServer{TCPPort: tcpPort, HTTPPort: httpPort}
+	fileServer.Start()
 }
